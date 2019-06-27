3ca484e59cda6f9e39cc4876d9d72575570f8ab7
Titov Kirill <kirill.titov@outlook.com>
2019-06-27 12:12:16 +0300

    Initial commit

---
+++ 3l_TitovKirill.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+
+struct car {
+    var color: String = " "
+    var model: String = " "
+    var amountOfWheels: Int = 4
+    var amountOfDoors: Int = 3
+}
+
+
+struct truck {
+    var color: String = " "
+    var model: String = " "
+    var amountOfWheels: Int = 4
+    var amountOfDoors: Int = 3
+}
+
+
+var BMW: car = car(color: <#T##String#>, model: <#T##String#>, amountOfWheels: <#T##Int#>, amountOfDoors: <#T##Int#>)
+
+
