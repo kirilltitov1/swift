24094d346358ac04fbff05608e982319e02be75d
Kirill <kirill.titov@outlook.com>
2019-08-03 22:54:03 +0300

    added UIScroll View

---
+++ Пользовательский интерфейс iOS-приложений/HW1/HW1 v2/HW1 v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW1 v2
+//
+//  Created by Kirill Titov on 03/08/2019.
+//  Copyright © 2019 Kirill Titov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
